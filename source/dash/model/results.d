be22061ad171970f5a2db175a641a866afc652b7
David Nadlinger <code@klickverbot.at>
2014-06-19 15:29:56 +0200

    Replace revision chooser page by redirect to a results page.
    This saves us from doing AJAX to fetch the run config
    selection. At some later point, we might want to migrate
    to a PJAX approach to reduce latency anyway, but right now,
    just always rendering the full page makes life simpler.
    
    Chooser not actually replaced with results page yet.

--- source/dash/model/results.d
+++ source/dash/model/results.d
@@ -3,6 +3,7 @@ module dash.model.results;
 import db = dash.model.db;
 import std.algorithm : map;
 import std.datetime : SysTime;
+import std.exception : enforce;
 import std.range;
 import vibe.data.bson;
 
@@ -17,6 +18,12 @@ class Results {
 
     string[] compilerNames() {
         return _db.compilers.find(Bson.emptyObject, ["name": 1]).map!(a => a["name"].get!string).array;
+    }
+
+    string[] runConfigNames(string compilerName) {
+        auto result = _db.compilers.findOne(["name": compilerName], ["runConfigs": 1]);
+        enforce(!result.isNull, "Unknown compiler.");
+        return result["runConfigs"].get!(Bson[]).map!(a => a["name"].get!string).array;
     }
 
 private:
--- source/dash/web/package.d
+++ source/dash/web/package.d
@@ -10,8 +10,8 @@ class WebFrontend {
 
     void registerRoutes(URLRouter r) {
         r.get("/", &showHome);
-        r.get("/compare", &showCompareChooser);
-        r.post("/compare", &redirectCompareResult);
+        r.get("/:machineName/compare", &redirectToDefaultCompare);
+        r.get("/:machineName/compare/:specifier", &getCompareResult);
         r.get("*", serveStaticFiles("./public"));
     }
 
@@ -19,30 +19,56 @@ class WebFrontend {
         renderStatic!"home.dt"(req, res);
     }
 
-    void showCompareChooser(HTTPServerRequest req, HTTPServerResponse res) {
+    void getCompareResult(HTTPServerRequest req, HTTPServerResponse res) {
         auto machineNames = _results.machineNames;
         auto compilerNames = _results.compilerNames;
+        auto defaultMachine = "<none>";
+        if (!machineNames.empty) defaultMachine = machineNames[0];
         res.render!(
             "compare_chooser.dt",
             compilerNames,
             machineNames,
+            defaultMachine,
             req
         );
     }
 
-    void redirectCompareResult(HTTPServerRequest req, HTTPServerResponse res) {
-        showCompareChooser(req, res);
+    void redirectToDefaultCompare(HTTPServerRequest req, HTTPServerResponse res) {
+        auto machineName = validatedMachineName(req);
+
+        auto compilers = _results.compilerNames;
+        enforce(!compilers.empty, new HTTPStatusException(HTTPStatus.notFound));
+        auto defaultCompiler = compilers[0];
+
+        auto runConfigs = _results.runConfigNames(defaultCompiler);
+        enforce(!runConfigs.empty, new HTTPStatusException(HTTPStatus.notFound));
+        auto defaultRunConfig = runConfigs[$ - 1];
+
+        res.redirect("%s%s/compare/%s:%s@current..%s:%s@previous".format(
+            req.rootDir, machineName, defaultCompiler, defaultRunConfig,
+            defaultCompiler, defaultRunConfig));
     }
 
 private:
     void renderStatic(string templateFile)(HTTPServerRequest req, HTTPServerResponse res) {
         // TODO: Cache!
         auto machineNames = _results.machineNames;
+        auto defaultMachine = "<none>";
+        if (!machineNames.empty) defaultMachine = machineNames[0];
         res.render!(
             templateFile,
             machineNames,
+            defaultMachine,
             req
         );
+    }
+
+    string validatedMachineName(HTTPServerRequest req) {
+        auto name = req.params["machineName"];
+        // Horribly inefficient, cache.
+        enforce(_results.machineNames.canFind(name),
+            new HTTPStatusException(HTTPStatus.notFound));
+        return name;
     }
 
     Results _results;
